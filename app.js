059d9f3a4739d45d8eefc3ac3b6503280dbb0f7d
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-07 17:25:06 +0000

    version4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var titles =document.getElementsByClassName("title");
 
- console.log(Array.isArray(titles));
- console.log(Array.isArray(Array.from(titles)));
+// console.log(Array.isArray(titles));
+//console.log(Array.isArray(Array.from(titles)));
  
-//titles.forEach(function(item){
-//    console.log(item);
-//})
+Array.from (titles).forEach(function(item){
+  console.log(item);
+})
