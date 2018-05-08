6843fce1860b2f1a504c5bccaf6f85da9a95f1c8
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-08 14:00:19 +0000

    version8

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:',bookList.parentNode);
-console.log('the parent element is',bookList.parentElement.parentElement);
+console.log('book-list next sibling is:',bookList.nextSibling);
+console.log('book-list next element sibling is:',bookList.nextElementSibling);
 
+console.log('book-list previous sibling is:',bookList.previousElementSibling);
+console.log('book-list previous element sibling is:',bookList.previousElementSibling);
 
-console.log(bookList.children);
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!'
