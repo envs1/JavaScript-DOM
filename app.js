d214ce7a30a660e5ab7fad2f345f32d781cd199b
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-08 13:33:40 +0000

    version5

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var titles =document.getElementsByClassName("title");
+var books = document.querySelectorAll('#book-list li .name');
 
-// console.log(Array.isArray(titles));
-//console.log(Array.isArray(Array.from(titles)));
- 
-Array.from (titles).forEach(function(item){
-  console.log(item);
-})
+Array.from(books).forEach(function(book){
+book.textContent += '(book title)';
+});
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2> books more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</P>';
