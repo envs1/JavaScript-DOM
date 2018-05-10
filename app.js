d885f2524c3e7de6b882c68ed006622cc02f58f7
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-10 09:00:36 +0000

    update

--- app.js
+++ app.js
@@ -42,4 +42,19 @@ hideBox.addEventListener('change', function(e){
   } else {
     list.style.display = "initial";
   }
+
+//filter books
+const searchBar = forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach((book) => {
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(e.target.value) != -1){
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
 });
