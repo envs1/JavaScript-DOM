21ca21a251199764cfff3ee56cc058ae4b309eff
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-10 07:24:32 +0000

    UPDATE fixed buttons

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //Event Bubbling
-var btns = document.querySelectorAll('#book-list.delete');
+const btns = document.querySelectorAll('#book-list ul li');
 
 Array.from(btns).forEach(function(btn){
     btn.addEventListener('click',function(e){
         
-        const li = e.target.parentElement;
+        const li =e.target.parentElement;
+        console.log('child element to remove',li);
+        console.log('parent element to remove child from:',li.parentElement);
         li.parentNode.removeChild(li);
         
     });
