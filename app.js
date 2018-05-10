64a65206caa455c245382d755404eb25bc1d7e34
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-10 08:04:43 +0000

    UPDATE Elements

--- app.js
+++ app.js
@@ -14,7 +14,22 @@ const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit',function(e){
    e.preventDefault();
-   const value= addForm.querySelector('input[type ="text"]').value;
-   console.log(value);
+   const value = addForm.querySelector('input[type ="text"]').value;
+  
+//create element
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+
+//add text content
+deleteBtn.textContent ='delete';
+bookName.textContent = value;
+//append to document
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
+
+    
     
 });
