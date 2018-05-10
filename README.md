008c0f8c5c3c8e938922a485973ba365f5afd58c
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-10 07:41:45 +0000

    UPDATE

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JavaScript-DOM
 
 version 10 update
+be able to delete via .delete not complete li
--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 //Event Bubbling
-const btns = document.querySelectorAll('#book-list ul li');
-
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        
+const list = document.querySelector('#book-list ul');
+//delete books
+list.addEventListener('click',function(e){
+    if(e.target.className =='delete'){
         const li =e.target.parentElement;
-        console.log('child element to remove',li);
-        console.log('parent element to remove child from:',li.parentElement);
         li.parentNode.removeChild(li);
         
-    });
+    }
 });
-
