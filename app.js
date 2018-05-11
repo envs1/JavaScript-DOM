c6e178b385d85c7640afb30e5b1b67f9ca5e8546
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-11 20:04:48 +0000

    update

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const list = document.querySelector('#book-list ul');
 const forms = document.forms;
 
 // delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
 });
 
 // add books
@@ -44,27 +44,27 @@ hideBox.addEventListener('change', function(e){
   }
 
 //filter books
-const searchBar = forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', (e) => {
-  const term = e.target.value.toLowerCase();
-  const books = list.getElementsByTagName('li');
-  Array.from(books).forEach((book) => {
-    const title = book.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(e.target.value) != -1){
-      book.style.display = 'block';
-    } else {
-      book.style.display = 'none';
-    }
-  });
-});
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    
+   Array.from(books).forEach(book => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
 });
 // tabbed content
 const tabs = document.querySelector('.tabs');
 const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', (e) => {
+tabs.addEventListener('click', (e)=>{
   if(e.target.tagName == 'LI'){
     const targetPanel = document.querySelector(e.target.dataset.target);
-    Array.from(panels).forEach((panel) => {
+    panels.from(panels).forEach((panel) => {
       if(panel == targetPanel){
         panel.classList.add('active');
       }else{
