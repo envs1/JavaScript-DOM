c8ebdd7b458d7478b15a1e102a4baf1de59edf00
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-08-01 02:47:02 +0000

    n

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var titles =document.getElementsByClassName("title");
 
  console.log(Array.isArray(titles));
  console.log(Array.isArray(Array.from(titles)));
- 
+ var x; 
 //titles.forEach(function(item){
 //    console.log(item);
 //})
